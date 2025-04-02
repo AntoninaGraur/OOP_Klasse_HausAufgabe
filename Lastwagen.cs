d9187ce59004bc2f1ef384b7f4141a175d809814
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2025-04-02 09:50:55 +0200

    Initial commit

---
+++ Lastwagen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Klasse_HausAufgabe
+{
+    class Lastwagen
+    {
+
+        private string Model;
+        private string Farbe;
+        private int Jahr;
+
+
+        public void setName(string Model, string Farbe, int Jahr)
+        {
+            this.Model = Model;
+            this.Farbe = Farbe;
+            this.Jahr = Jahr;
+        }
+
+        public string GetModel()
+        {
+            return this.Model;
+        }
+
+        public string GetFarbe()
+        {
+            return this.Farbe;
+        }
+
+        public int GetJahr()
+        {
+            return this.Jahr;
+        }
+
+        public Lastwagen(string model, string farbe, int jahr )
+        {
+            this.Model=model;   
+            this.Farbe = farbe;
+            this.Jahr = jahr;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using OOP_Klasse_HausAufgabe;
+
+class Programm
+{
+    static void Main()
+    {
+
+        Console.Write("Gib das Model ein:  ");
+        string model = Console.ReadLine();
+
+        Console.Write("Gib die Farbe ein:  ");
+        string farbe = Console.ReadLine();
+
+        Console.Write("Gib das Baujahr ein:  ");
+        int jahr;
+
+
+        while (!int.TryParse(Console.ReadLine(), out jahr))
+        {
+            Console.Write("Ungültige Eingabe!");
+        }
+
+        Lastwagen lastwagen1 = new(model, farbe, jahr);
+
+        Console.WriteLine("\n Lastwagen Details" );
+        Console.WriteLine("\n Model: " + lastwagen1.GetModel());
+        Console.WriteLine("Farbe: " + lastwagen1.GetFarbe());
+        Console.WriteLine("Baujahr: " + lastwagen1.GetJahr());
+
+
+
+
+    }
+}
